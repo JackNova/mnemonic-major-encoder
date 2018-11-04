e88352054284f3df27bdda17b8c68289d728ad8e
Dario Iacampo <dario@jntstudio.net>
2018-11-04 09:29:44 +0100

    Bump version: 0.1.6 → 0.1.7

--- mnemonic_major_encoder/__init__.py
+++ mnemonic_major_encoder/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Dario Iacampo"""
 __email__ = 'dario@jntstudio.net'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/JackNova/mnemonic_major_encoder',
-    version='0.1.6',
+    version='0.1.7',
     zip_safe=False,
 )
